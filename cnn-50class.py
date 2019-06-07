c90f603aead6c92a7abe55a1e90d3ea5cc897010
Md. Asifuzzaman Jishan <32738421+jishan900@users.noreply.github.com>
2019-06-08 02:12:45 +0600

    Add files via upload

---
+++ cnn-50class.py
@@ -0,0 +1,455 @@
+# Import libraries
+import os,cv2
+import numpy as np
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
+from keras import backend as K
+K.set_image_dim_ordering('th')
+
+from keras.applications import VGG16
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD,RMSprop,adam
+from keras import optimizers
+import glob
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, Convolution2D, Dropout, LSTM, TimeDistributed, Embedding, Bidirectional, Activation, RepeatVector
+
+
+PATH = os.getcwd()
+# Define data path
+data_path = PATH + '/data'
+data_dir_list = os.listdir(data_path)
+
+img_rows=224
+img_cols=224
+num_channel=1
+num_epoch=10
+
+# Define the number of classes
+num_classes = 50
+
+img_data_list=[]
+
+for dataset in data_dir_list:
+	img_list=os.listdir(data_path+'/'+ dataset)
+	print ('Loaded the images of dataset-'+'{}\n'.format(dataset))
+	for img in img_list:
+		input_img=cv2.imread(data_path + '/'+ dataset + '/'+ img )
+		input_img=cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+		input_img_resize=cv2.resize(input_img,(224,224))
+		img_data_list.append(input_img_resize)
+
+img_data = np.array(img_data_list)
+img_data = img_data.astype('float32')
+img_data /= 255
+print (img_data.shape)
+
+if num_channel==1:
+	if K.image_dim_ordering()=='th':
+		img_data= np.expand_dims(img_data, axis=1) 
+		print (img_data.shape)
+	else:
+		img_data= np.expand_dims(img_data, axis=4) 
+		print (img_data.shape)
+		
+else:
+	if K.image_dim_ordering()=='th':
+		img_data=np.rollaxis(img_data,3,1)
+		print (img_data.shape)
+		
+#%%
+USE_SKLEARN_PREPROCESSING=False
+
+if USE_SKLEARN_PREPROCESSING:
+	# using sklearn for preprocessing
+	from sklearn import preprocessing
+	
+	def image_to_feature_vector(image, size=(224, 224)):
+		# resize the image to a fixed size, then flatten the image into
+		# a list of raw pixel intensities
+		return cv2.resize(image, size).flatten()
+	
+	img_data_list=[]
+	for dataset in data_dir_list:
+		img_list=os.listdir(data_path+'/'+ dataset)
+		print ('Loaded the images of dataset-'+'{}\n'.format(dataset))
+		for img in img_list:
+			input_img=cv2.imread(data_path + '/'+ dataset + '/'+ img )
+			input_img=cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+			input_img_flatten=image_to_feature_vector(input_img,(224,224))
+			img_data_list.append(input_img_flatten)
+	
+	img_data = np.array(img_data_list)
+	img_data = img_data.astype('float32')
+	print (img_data.shape)
+	img_data_scaled = preprocessing.scale(img_data)
+	print (img_data_scaled.shape)
+	
+	print (np.mean(img_data_scaled))
+	print (np.std(img_data_scaled))
+	
+	print (img_data_scaled.mean(axis=0))
+	print (img_data_scaled.std(axis=0))
+	
+	if K.image_dim_ordering()=='th':
+		img_data_scaled=img_data_scaled.reshape(img_data.shape[0],num_channel,img_rows,img_cols)
+		print (img_data_scaled.shape)
+		
+	else:
+		img_data_scaled=img_data_scaled.reshape(img_data.shape[0],img_rows,img_cols,num_channel)
+		print (img_data_scaled.shape)
+	
+	
+	if K.image_dim_ordering()=='th':
+		img_data_scaled=img_data_scaled.reshape(img_data.shape[0],num_channel,img_rows,img_cols)
+		print (img_data_scaled.shape)
+		
+	else:
+		img_data_scaled=img_data_scaled.reshape(img_data.shape[0],img_rows,img_cols,num_channel)
+		print (img_data_scaled.shape)
+
+if USE_SKLEARN_PREPROCESSING:
+	img_data=img_data_scaled
+#%%
+# Assigning Labels
+
+# Define the number of classes
+num_classes = 10
+
+num_of_samples = img_data.shape[0]
+labels = np.ones((num_of_samples,),dtype='int64')
+
+labels[0:202]=0
+labels[202:404]=1
+labels[404:606]=2
+labels[606:808]=3
+labels[808:1010]=4
+labels[1010:1212]=5
+labels[1212:1414]=6
+labels[1414:1616]=7
+labels[1616:1818]=8
+labels[1818:2020]=9
+labels[2020:2222]=10
+labels[2222:2424]=11
+labels[2424:2626]=12
+labels[2626:2828]=13
+labels[2828:3030]=14
+labels[3030:3232]=15
+labels[3232:3434]=16
+labels[3434:3636]=17
+labels[3636:3838]=18
+labels[3838:4040]=19
+labels[4040:4242]=20
+labels[4242:4444]=21
+labels[4444:4646]=22
+labels[4646:4848]=23
+labels[4848:5050]=24
+labels[5050:5252]=25
+labels[5252:5454]=26
+labels[5454:5656]=27
+labels[5656:5858]=28
+labels[5858:6060]=29
+labels[6060:6262]=30
+labels[6262:6464]=31
+labels[6464:6666]=32
+labels[6666:6868]=33
+labels[6868:7070]=34
+labels[7070:7272]=35
+labels[7272:7474]=36
+labels[7474:7676]=37
+labels[7676:7878]=38
+labels[7878:8080]=39
+labels[8080:8282]=40
+labels[8282:8484]=41
+labels[8484:8686]=42
+labels[8686:8888]=43
+labels[8888:9090]=44
+labels[9090:9292]=45
+labels[9292:9494]=46
+labels[9494:9696]=47
+labels[9696:9898]=48
+labels[9898:10100]=49
+labels[10100:]=50
+  
+names = ['cat','dog','horse','human','house','nature','hill','sea','building','cow','window','village','town','chair','table','board','spoon','cake','mirror','bottle','pen','pencil','book','car','truck','sky','train','bus','areoplane','bird','tree','fish','water','flower','grass','baby','stone','wall','hand','shirt','jeans','chilli','women','men','cng','boat','iron','river','leaf','air conditioner']
+	  
+# convert class labels to on-hot encoding
+Y = np_utils.to_categorical(labels, num_classes)
+
+#Shuffle the dataset
+x,y = shuffle(img_data,Y, random_state=2)
+# Split the dataset
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
+
+#%%
+# Defining the model
+input_shape=img_data[0].shape
+					
+model = Sequential()
+
+model.add(Convolution2D(32, 3,3,border_mode='same',input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(Convolution2D(32, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Convolution2D(64, 3, 3))
+model.add(Activation('relu'))
+#model.add(Convolution2D(64, 3, 3))
+#model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer='sgd',metrics=["accuracy"])
+#model.compile(loss='categorical_crossentropy', optimizer='rmsprop',metrics=["accuracy"])
+
+# Viewing model_configuration
+
+model.summary()
+model.get_config()
+model.layers[0].get_config()
+model.layers[0].input_shape			
+model.layers[0].output_shape			
+model.layers[0].get_weights()
+np.shape(model.layers[0].get_weights()[0])
+model.layers[0].trainable
+
+#%%
+# Training
+hist = model.fit(X_train, y_train, batch_size=16, nb_epoch=num_epoch, verbose=1, validation_data=(X_test, y_test))
+
+#hist = model.fit(X_train, y_train, batch_size=32, nb_epoch=20,verbose=1, validation_split=0.2)
+
+# Training with callbacks
+from keras import callbacks
+
+filename='/home/jishan/Desktop/TEST-F-CNN-D-FD/model-csv/model_train_new.csv'
+csv_log=callbacks.CSVLogger(filename, separator=',', append=False)
+
+early_stopping=callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=0, mode='min')
+
+filepath="/home/jishan/Desktop/TEST-F-CNN-D-FD/cv-cnn-model/Best-weights-my_model-{epoch:03d}-{loss:.4f}-{acc:.4f}.hdf5"
+
+checkpoint = callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+
+callbacks_list = [csv_log,early_stopping,checkpoint]
+
+hist = model.fit(X_train, y_train, batch_size=16, nb_epoch=num_epoch, verbose=1, validation_data=(X_test, y_test),callbacks=callbacks_list)
+
+
+# visualizing losses and accuracy
+train_loss=hist.history['loss']
+val_loss=hist.history['val_loss']
+train_acc=hist.history['acc']
+val_acc=hist.history['val_acc']
+xc=range(num_epoch)
+
+plt.figure(1,figsize=(7,5))
+plt.plot(xc,train_loss)
+plt.plot(xc,val_loss)
+plt.xlabel('num of Epochs')
+plt.ylabel('loss')
+plt.title('train_loss vs val_loss')
+plt.grid(True)
+plt.legend(['train','val'])
+plt.savefig('/home/jishan/Desktop/TEST-F-CNN-D-FD/plot/loss_graph')
+
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+
+plt.figure(2,figsize=(7,5))
+plt.plot(xc,train_acc)
+plt.plot(xc,val_acc)
+plt.xlabel('num of Epochs')
+plt.ylabel('accuracy')
+plt.title('train_acc vs val_acc')
+plt.grid(True)
+plt.legend(['train','val'],loc=4)
+plt.savefig('/home/jishan/Desktop/TEST-F-CNN-D-FD/plot/accuracy_graph')
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+
+#%%
+
+
+
+# Testing a new image
+test_image = cv2.imread('data/Dataset/0121.png')
+test_image=cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
+test_image=cv2.resize(test_image,(224,224))
+test_image = np.array(test_image)
+test_image = test_image.astype('float32')
+test_image /= 255
+print (test_image.shape)
+   
+if num_channel==1:
+	if K.image_dim_ordering()=='th':
+		test_image= np.expand_dims(test_image, axis=0)
+		test_image= np.expand_dims(test_image, axis=0)
+		print (test_image.shape)
+	else:
+		test_image= np.expand_dims(test_image, axis=3) 
+		test_image= np.expand_dims(test_image, axis=0)
+		print (test_image.shape)
+		
+else:
+	if K.image_dim_ordering()=='th':
+		test_image=np.rollaxis(test_image,2,0)
+		test_image= np.expand_dims(test_image, axis=0)
+		print (test_image.shape)
+	else:
+		test_image= np.expand_dims(test_image, axis=0)
+		print (test_image.shape)
+		
+# Predicting the test image
+print((model.predict(test_image)))
+print(model.predict_classes(test_image))
+
+#%%
+
+# Visualizing the intermediate layer
+
+#
+def get_featuremaps(model, layer_idx, X_batch):
+	get_activations = K.function([model.layers[0].input, K.learning_phase()],[model.layers[layer_idx].output,])
+	activations = get_activations([X_batch,0])
+	return activations
+
+layer_num=3
+filter_num=0
+
+activations = get_featuremaps(model, int(layer_num),test_image)
+
+print (np.shape(activations))
+feature_maps = activations[0][0]      
+print (np.shape(feature_maps))
+
+if K.image_dim_ordering()=='th':
+	feature_maps=np.rollaxis((np.rollaxis(feature_maps,2,0)),2,0)
+print (feature_maps.shape)
+
+fig=plt.figure(figsize=(16,16))
+plt.imshow(feature_maps[:,:,filter_num],cmap='gray')
+plt.savefig("/home/jishan/Desktop/TEST-F-CNN-D-FD/featuremap/featuremaps-layer-{}".format(layer_num) + "-filternum-{}".format(filter_num)+'.jpg')
+
+num_of_featuremaps=feature_maps.shape[2]
+fig=plt.figure(figsize=(16,16))	
+plt.title("featuremaps-layer-{}".format(layer_num))
+subplot_num=int(np.ceil(np.sqrt(num_of_featuremaps)))
+for i in range(int(num_of_featuremaps)):
+	ax = fig.add_subplot(subplot_num, subplot_num, i+1)
+	#ax.imshow(output_image[0,:,:,i],interpolation='nearest' ) #to see the first filter
+	ax.imshow(feature_maps[:,:,i],cmap='gray')
+	plt.xticks([])
+	plt.yticks([])
+	plt.tight_layout()
+plt.show()
+fig.savefig("/home/jishan/Desktop/TEST-F-CNN-D-FD/featuremap/featuremaps-layer-{}".format(layer_num) + '.jpg')
+
+#%%
+# Printing the confusion matrix
+from sklearn.metrics import classification_report,confusion_matrix
+import itertools
+
+Y_pred = model.predict(X_test)
+print(Y_pred)
+y_pred = np.argmax(Y_pred, axis=1)
+print(y_pred)
+#y_pred = model.predict_classes(X_test)
+#print(y_pred)
+target_names = ['class 0(cat)', 'class 1(dog)', 'class 2(horse)','class 3(human)','class 4(house)','class 5(nature)','class 6(hill)','class 7(sea)','class 8(building)','class 9(cow)', 'class 10(window)', 'class 11(village)', 'class 12(town)','class 13(chair)','class 14(table)','class 15(board)','class 16(spoon)','class 17(cake)','class 18(mirror)','class 19(bottle)', 'class 20(pen)', 'class 21(pencil)', 'class 22(book)','class 23(car)','class 24(truck)','class 25(sky)','class 26(train)','class 27(bus)','class 28(areoplane)','class 29(bird)', 'class 30(tree)', 'class 31(fish)', 'class 32(water)','class 33(flower)','class 34(grass)','class 35(baby)','class 36(stone)','class 37(wall)','class 38(hand)','class 39(shirt)', 'class 40(jeans)', 'class 41(chilli)', 'class 42(women)','class 43(men)','class 44(cng)','class 45(boat)','class 46(iron)','class 47(river)','class 48(leaf)','class 49(air conditioner)']
+				
+
+# Plotting the confusion matrix
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+# Compute confusion matrix
+cnf_matrix = (confusion_matrix(np.argmax(y_test,axis=1), y_pred))
+
+np.set_printoptions(precision=2)
+
+plt.figure()
+
+# Plot non-normalized confusion matrix
+plot_confusion_matrix(cnf_matrix, classes=target_names,
+                      title='Confusion matrix')
+#plt.figure()
+# Plot normalized confusion matrix
+#plot_confusion_matrix(cnf_matrix, classes=target_names, normalize=True,
+#                      title='Normalized confusion matrix')
+#plt.figure()
+plt.savefig('/home/jishan/Desktop/TEST-F-CNN-D-FD/confusion_matrix/confusion_matrix')
+plt.show()
+
+#%%
+# Saving and loading model and weights
+from keras.models import model_from_json
+from keras.models import load_model
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
+
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+
+model.save('model.hdf5')
+loaded_model=load_model('model.hdf5')
